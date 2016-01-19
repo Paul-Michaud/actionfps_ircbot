1b56ad5b91ff3d34870af787f80a5aef8bc4474f
Paul Michaud <paul.michaud@fr.clara.net>
2016-01-19 15:25:25 +0100

    ajout bot quakenet

---
+++ botquakenet.py
@@ -0,0 +1,32 @@
+import irclib
+import ircbot
+import json
+from sseclient import SSEClient
+
+print "Bot started"
+
+class BotQuakenet(ircbot.SingleServerIRCBot):
+    def __init__(self):
+        ircbot.SingleServerIRCBot.__init__(self, [('euroserv.fr.quakenet.org', 6667)],
+                                           'ActionFPS_BOT', 'ActionFPS inter bot')
+    def on_welcome(self, serv, ev):
+	channels = ['#million'];
+	for i in range(0,len(channels)):
+    		print 'Bot joined ' + channels[i]
+        	serv.join(channels[i])
+	while True:
+	        messages = SSEClient('http://woop.ac:81/actionfps/inters/')
+        	for message in messages:
+                	if message.data:
+                        	data = json.loads(message.data)
+	                        serverName = str(data['serverName'])
+        	                playerName = data['playerName']
+                	        serverConnect = data['serverConnect']
+                        	event = message.event
+	                        msg = playerName + ' started an ' + event + ' and is looking for players on ' + serverName
+        	                msg = str(msg)
+                	        print msg
+				for i in range(0,len(channels)):
+			                serv.privmsg(channels[i], msg)
+
+BotQuakenet().start()
